cff8452b20782d5b0802a28f1555411da15b1bcb
Jelani Woods <jelani@firstdraft.com>
2020-10-10 14:39:36 -0500

    Remove duplicate test

--- spec/scripts/date_spec.rb
+++ spec/scripts/date_spec.rb
@@ -47,19 +47,6 @@ describe "date_formatted.rb" do
   end
 end
 
-describe "date_formatted.rb" do
-  it "should output 'Is today Monday? false', when today is not Monday", points: 1 do
-    # Un-require date_formatted.rb
-    date_formatted_file = $".select{|r| r.include? 'date_formatted.rb'}
-    $".delete(date_formatted_file.first)
-
-    # Set today's date to Monday- 2020-06-29
-    allow(Date).to receive(:today).and_return Date.new(2020,06,29)
-    
-
-    expect { require_relative '../../date_formatted' }.to output(/The year is: 2020, the calendar day is: 29, and the month is: 6/).to_stdout
-  end
-end
 
 describe "date_monday.rb" do
   it "should output 'Is today Monday? false', when today is not Monday", points: 1 do
